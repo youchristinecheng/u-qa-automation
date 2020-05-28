091bfeed2c219c9e33f5457dc5e3ef82cd68c446
tsemark-you <54795209+tsemark-you@users.noreply.github.com>
2020-05-28 18:18:15 +0800

    Slackmsg (#15)
    added slack integration

--- .buildkite/scripts/send-slack-notification.sh
+++ .buildkite/scripts/send-slack-notification.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+## 1. Attachmend; 2. ENV; 3. PLATFORM; 4.BUILDVER; 5. STATUS
 
 SLACK_OAUTH_TOKEN=$BUILDKITE_SLACK_TOKEN
-FILE_PATH=./target/surefire-reports/emailable-report.html
+FILE_PATH=$1
 SLACK_CHANNEL=buildkite_test_report
 
-curl -F file=$FILE_PATH -F channels=$SLACK_CHANNEL -H "Authorization: Bearer $SLACK_OAUTH_TOKEN" https://slack.com/api/files.upload
+## MODIFY JSON DATA
+if [ "$5" == "true" ]; then RESULT=36a64f; else  RESULT=ff0000; fi
+
+sed -e "s/<ENV>/$2/g; s/<PLATFORM>/$3/g; s/<BUILDVER>/$4/g; s/<BUILDNUM>/$BUILDKITE_BUILD_NUMBER/g; s/<COLOR>/$RESULT/g" slackmsg_template.json > slackmsg.json
+curl -H "Content-Type: application/json" --data @slackmsg.json -H "Authorization: Bearer $SLACK_OAUTH_TOKEN" https://slack.com/api/chat.postMessage
+curl -F "file=@$FILE_PATH" -F title=HTML_REPORT_FILE -F channels=$SLACK_CHANNEL -H "Authorization: Bearer $SLACK_OAUTH_TOKEN" https://slack.com/api/files.upload
